759deaff9a236ffaec51fb49002ec63226ccf2d4
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-04-06 19:48:17 +0200

    Instrukcja konfiguracji

--- config/__init__.py
+++ config/__init__.py
@@ -1,40 +1,20 @@
-'''Plik konfiguracyjny'''
+"""Plik konfiguracyjny"""
 __author__ = 'Patryk Niedźwiedziński'
 
-class Localhost:
-    '''Do testowania na komputerze lokalnym'''
+class Config():
+    """Config class"""
     secret_key = str()
     DEBUG = True
     HOST = "127.0.0.1"
     PORT = 5000
+    EMAIL = ""
+    EMAIL_PASSWORD = ""
 
     def __init__(self, secret):
         self.secret_key = secret
 
     def is_ok(self):
-        '''Sprawdza czy dane poprawne'''
-        import socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((self.HOST, self.PORT))
-        if result==0:
-            sock.close()
-            return False
-        else:
-            sock.close()
-            return True
-
-class Server:
-    '''Do uruchamiania na serwerze'''
-    secret_key = str()
-    DEBUG = True
-    HOST = "127.0.0.1"
-    PORT = 80
-
-    def __init__(self, secret):
-        self.secret_key = secret
-
-    def is_ok(self):
-        '''Sprawdza czy dane poprawne'''
+        """Check if host and port are valid"""
         import socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((self.HOST, self.PORT))
@@ -46,4 +26,4 @@ class Server:
             return True
 
 
-CONFIG = Localhost("sekretny_klucz")
+CONFIG = Config("sekretny_klucz")
