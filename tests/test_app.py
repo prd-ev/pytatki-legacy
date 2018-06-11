38daa845fce5f13592bb093853e7299673503ac8
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-06-11 11:58:51 +0200

    Fix some errors

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,11 @@
+"""Test of application"""
 from flask import url_for
 
-class TestApp:
 
-    def test_ping(self, client):
-        res = client.get(url_for('_ping'))
-        if not res.status_code == 200:
-            raise AssertionError()
-        if not res.json == {'ping': 'pong'}:
-            raise AssertionError()
+def test_ping(client):
+    """Tests if application works"""
+    res = client.get(url_for('_ping'))
+    if not res.status_code == 200:
+        raise AssertionError()
+    if not res.json == {'ping': 'pong'}:
+        raise AssertionError()
