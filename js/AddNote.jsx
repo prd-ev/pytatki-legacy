897acee81cbce75016353aabbee071c0df459702
Filip Wachowiak <filipw01@interia.eu>
2018-07-23 15:01:24 +0200

    Added input form to new topic and new subject in add note. Added onclicks when navigating in notes

--- js/AddNote.jsx
+++ js/AddNote.jsx
@@ -5,7 +5,8 @@ class AddNote extends React.Component {
     super(props);
     this.state = {
       current_topics: [],
-      subject: null
+      subject_input: false,
+      topic_input: false
     };
   }
 
@@ -42,18 +43,6 @@ class AddNote extends React.Component {
     this.props.update(updated_notes);
   };
 
-  packNotes = () => {
-    let notatki = [];
-    for (let value of this.props.notatki) {
-      notatki.push(
-        <h3 key={value.substring(value.lastIndexOf("/") + 1)}>
-          {value.substring(value.lastIndexOf("/") + 1)}
-        </h3>
-      );
-    }
-    return notatki;
-  };
-
   packTopicOptions = () => {
     let topic_options_temp = [];
     let topic_options = [];
@@ -82,6 +71,9 @@ class AddNote extends React.Component {
     for (let value of topic_options_temp) {
       topic_options.push(<option key={value}>{value}</option>);
     }
+    topic_options.push(
+      <option key="--Dodaj nowy dział--">--Dodaj nowy dział--</option>
+    );
     if (this.state.current_topics !== topic_options) {
       this.setState({
         current_topics: topic_options
@@ -112,6 +104,9 @@ class AddNote extends React.Component {
     for (let value of subject_options_temp) {
       subject_options.push(<option key={value}>{value}</option>);
     }
+    subject_options.push(
+      <option key="--Dodaj nowy przedmiot--">--Dodaj nowy przedmiot--</option>
+    );
     return subject_options;
   };
 
@@ -121,6 +116,43 @@ class AddNote extends React.Component {
 
   subjectChange = () => {
     this.packTopicOptions();
+    if (
+      document.getElementById("subject").value === "--Dodaj nowy przedmiot--"
+    ) {
+      this.setState({
+        subject_input: true,
+        topic_input: true
+      });
+    } else {
+      this.setState({
+        subject_input: false
+      });
+    }
+    
+  };
+
+  topicChange = () => {
+    if (document.getElementById("topic").value === "--Dodaj nowy dział--") {
+      this.setState({
+        topic_input: true
+      });
+    } else {
+      this.setState({
+        topic_input: false
+      });
+    }
+  };
+
+  newSubjectInput = () => {
+    if (this.state.subject_input) {
+      return <input type="text" name="new-subject" />;
+    }
+  };
+
+  newTopicInput = () => {
+    if (this.state.topic_input) {
+      return <input type="text" name="new-topic" />;
+    }
   };
 
   render() {
@@ -131,12 +163,15 @@ class AddNote extends React.Component {
           <select id="subject" onChange={this.subjectChange}>
             {this.packSubjectOptions()}
           </select>
+          {this.newSubjectInput()}
           Dział
-          <select id="topic">{this.state.current_topics}</select>
+          <select id="topic" onChange={this.topicChange}>
+            {this.state.current_topics}
+          </select>
+          {this.newTopicInput()}
           Nazwa notatki <input type="text" id="note" />
           <input type="submit" value="Dodaj notatkę" />
         </form>
-        {this.packNotes()}
       </div>
     );
   }
--- js/Notatki.jsx
+++ js/Notatki.jsx
@@ -6,7 +6,10 @@ class Notatki extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      notatki: data
+      notatki: data,
+      subjects: null,
+      current_subject: null,
+      current_topic: null
     };
   }
 
@@ -16,10 +19,107 @@ class Notatki extends React.Component {
     }));
   };
 
+  changeCurrentSubject = e => {
+    this.setState({
+      current_subject: e.target.className,
+      current_topic: null
+    });
+  };
+
+  changeCurrentTopic = e => {
+    this.setState({
+      current_topic: e.target.className
+    });
+  };
+
+  packSubjects = () => {
+    let subjects = [];
+    let subjects_temp = [];
+    for (let temp_value of this.state.notatki) {
+      if (
+        subjects_temp.indexOf(
+          temp_value.substring(
+            temp_value.indexOf("/") + 1,
+            temp_value.indexOf("/", 1)
+          )
+        ) < 0
+      ) {
+        subjects_temp.push(
+          temp_value.substring(
+            temp_value.indexOf("/") + 1,
+            temp_value.indexOf("/", 1)
+          )
+        );
+      }
+    }
+    for (let value of subjects_temp) {
+      subjects.push(
+        <h1 className={value} onClick={this.changeCurrentSubject} key={value}>
+          {value}
+        </h1>
+      );
+    }
+    return subjects;
+  };
+
+  packTopics = () => {
+    let topics_temp = [];
+    let topics = [];
+    for (let temp_value of this.state.notatki) {
+      if (
+        topics_temp.indexOf(
+          temp_value.substring(
+            temp_value.indexOf("/", 1) + 1,
+            temp_value.lastIndexOf("/")
+          )
+        ) < 0 &&
+        temp_value.substring(
+          temp_value.indexOf("/") + 1,
+          temp_value.indexOf("/", 1)
+        ) === this.state.current_subject
+      ) {
+        topics_temp.push(
+          temp_value.substring(
+            temp_value.indexOf("/", 1) + 1,
+            temp_value.lastIndexOf("/")
+          )
+        );
+      }
+    }
+    for (let value of topics_temp) {
+      topics.push(
+        <h2 className={value} onClick={this.changeCurrentTopic} key={value}>
+          {value}
+        </h2>
+      );
+    }
+    return topics;
+  };
+
+  packNotes = () => {
+    let notatki = [];
+    for (let value of this.state.notatki) {
+      if (
+        value.substring(
+          value.indexOf("/", 1) + 1,
+          value.lastIndexOf("/")
+        ) === this.state.current_topic
+      ) {
+        notatki.push(
+          <h3 key={value}>{value.substring(value.lastIndexOf("/") + 1)}</h3>
+        );
+      }
+    }
+    return notatki;
+  };
+
   render() {
     return (
       <div>
         <AddNote notatki={this.state.notatki} update={this.updateNotes} />
+        {this.packSubjects()}
+        {this.packTopics()}
+        {this.packNotes()}
       </div>
     );
   }
