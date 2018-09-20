2b30bab678a66d18d05f475d990faa494cd55de8
Filip Wachowiak <filipw01@interia.eu>
2018-09-20 23:11:57 +0200

    GraphQL basic query execution

--- pytatki/api/graphql/__init__.py
+++ pytatki/api/graphql/__init__.py
@@ -1,6 +1,10 @@
 from flask_graphql import GraphQLView
 from main import APP
 
+from dbconnect import connection
+from pymysql import escape_string
+import gc
+
 from graphql.type.definition import GraphQLArgument, GraphQLField, GraphQLNonNull, GraphQLObjectType
 from graphql.type.scalars import GraphQLString
 from graphql.type.schema import GraphQLSchema
@@ -9,6 +13,14 @@ from graphql.type.schema import GraphQLSchema
 def resolve_raises(*_):
     raise Exception("Throws!")
 
+def user(query):
+    con, conn = connection()
+    con.execute(query)
+    result = con.fetchone()
+    con.close()
+    conn.close()
+    gc.collect()
+    return result
 
 QueryRootType = GraphQLObjectType(
     name='QueryRoot',
@@ -24,6 +36,10 @@ QueryRootType = GraphQLObjectType(
                 'who': GraphQLArgument(GraphQLString)
             },
             resolver=lambda obj, info, who='World': 'Hello %s' % who
+        ),
+        'sayHello': GraphQLField(
+            type=GraphQLString,
+            resolver=lambda obj, info: user("SELECT * FROM user")
         )
     }
 )
@@ -40,5 +56,5 @@ MutationRootType = GraphQLObjectType(
 
 schema = GraphQLSchema(QueryRootType, MutationRootType)
 
-
-APP.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+APP.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema))
+APP.add_url_rule('/graphiql', view_func=GraphQLView.as_view('grapihql', schema=schema, graphiql=True))
