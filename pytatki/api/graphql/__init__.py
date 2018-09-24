0ef0c6846eab82f1ee5fef46598e002899d2f5a8
Filip Wachowiak <filipw01@interia.eu>
2018-09-24 11:10:04 +0200

    First working GraphQL for root folders and getNoteByParentId

--- pytatki/api/graphql/__init__.py
+++ pytatki/api/graphql/__init__.py
@@ -6,6 +6,8 @@ from pymysql import escape_string
 import gc
 import json
 
+from pytatki.views import find_usergroup_children
+
 from graphql.type.definition import GraphQLArgument, GraphQLField, GraphQLNonNull, GraphQLObjectType
 from graphql.type.scalars import GraphQLString, GraphQLInt
 from graphql.type.schema import GraphQLSchema
@@ -14,7 +16,7 @@ from graphql.type.schema import GraphQLSchema
 def resolve_raises(*_):
     raise Exception("Throws!")
 
-def user(query):
+def executeSQL(query):
     con, conn = connection()
     con.execute(query)
     result = con.fetchone()
@@ -26,19 +28,27 @@ def user(query):
 QueryRootType = GraphQLObjectType(
     name='QueryRoot',
     fields={
-        'test': GraphQLField(
-            type=GraphQLString,
-            args={
-                'who': GraphQLArgument(GraphQLString)
-            },
-            resolver=lambda obj, info, who='World': 'Hello %s' % who
-        ),
         'getUser': GraphQLField(
             type=GraphQLString,
             args={
                 'ident': GraphQLArgument(GraphQLInt)
             },
-            resolver=lambda obj, info, ident: user("SELECT * FROM user WHERE iduser = %i" % ident)
+            resolver=lambda obj, info, ident: executeSQL("SELECT * FROM user WHERE iduser = %i" % ident)
+        ),
+        'getNoteByParentId': GraphQLField(
+            type=GraphQLString,
+            args={
+                'parent_id': GraphQLArgument(GraphQLInt)
+            },
+            resolver=lambda obj, info, parent_id: executeSQL("SELECT title FROM note_view WHERE parent_id = %i" % parent_id)
+        ),
+        'getRootFolders': GraphQLField(
+            type=GraphQLString,
+            args={
+                'id_usergroup': GraphQLArgument(GraphQLInt),
+                'id_user': GraphQLArgument(GraphQLInt)
+            },
+            resolver=lambda obj, info, id_usergroup, id_user: find_usergroup_children(id_usergroup, id_user)
         )
     }
 )
@@ -56,4 +66,3 @@ MutationRootType = GraphQLObjectType(
 schema = GraphQLSchema(QueryRootType, MutationRootType)
 
 APP.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema))
-APP.add_url_rule('/graphiql', view_func=GraphQLView.as_view('grapihql', schema=schema, graphiql=True))
