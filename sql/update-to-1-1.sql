a12130f15e6e05ff6377b6877033d8065de1ccf5
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-09-26 22:14:04 +0200

    Fix DBv1.1

--- sql/update-to-1-1.sql
+++ sql/update-to-1-1.sql
@@ -3,11 +3,13 @@
 -- Source-Model-Version: 1.0
 -- Model: Pytatki    Target-Version: 1.1
 
-DROP VIEW note_view;
+USE `pytatki`;
+
+DROP VIEW `pytatki`.`note_view`;
 
 DROP TABLE IF EXISTS `pytatki`.`note_view`;
 USE `pytatki`;
 CREATE  OR REPLACE VIEW `note_view` AS
-SELECT a.idnote, a.value, a.title, b.name AS 'note_type', a.user_id AS 'creator_id', d.login AS 'creator_login', a.notegroup_id, c.name AS 'notegroup_name'
+SELECT a.idnote, a.value, a.title, b.name AS 'note_type', a.user_id AS 'creator_id', d.login AS 'creator_login', a.notegroup_id, c.folder_name AS 'notegroup_name'
 FROM note a, note_type b, notegroup_view c, usergroup_membership d
 WHERE a.note_type_id = b.idnote_type AND a.user_id = c.iduser AND a.notegroup_id = c.idnotegroup AND d.idusergroup = c.idusergroup;
