430515e8a135dd920a15ce0b80fb33c112f017d2
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-09-30 11:39:21 +0200

    README update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Pytatki',
-    version="1.0a0",
+    version="1.0a0.post1",
     description="Organizer klasowy",
     long_description=long_description,
     long_description_content_type="text/markdown",
