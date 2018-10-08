8c75aca41d5c4577ad79c5602fe27de8897d6b38
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-10-08 16:51:36 +0200

    Fix api tests

--- tests/test_api_functions.py
+++ tests/test_api_functions.py
@@ -2,11 +2,11 @@ import pytest
 import pymysql
 from pytatki.dbconnect import connection, create_user, create_status, create_usergroup, add_user_to_usergroup, create_notegroup, create_note_type, create_note
 from passlib.hash import sha256_crypt
-from pytatki.views import has_access_to_note
+from pytatki.views import has_access_to_note, type_id, has_access_to_usergroup
 from init_db import parse_sql, db_init
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def create_db():
     conn = pymysql.connect(host='127.0.0.1', user='root',
                            charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
@@ -19,7 +19,8 @@ def create_db():
         cursor.execute(query)
     conn.close()
 
-@pytest.fixture(autouse=True)
+
+@pytest.fixture(scope='session', autouse=True)
 def insert_status(create_db):
     _, conn = connection()
     conn.begin()
@@ -28,7 +29,8 @@ def insert_status(create_db):
     _.close()
     conn.close()
 
-@pytest.fixture(autouse=True)
+
+@pytest.fixture(scope='session', autouse=True)
 def insert_user(insert_status):
     _, conn = connection()
     conn.begin()
@@ -38,7 +40,8 @@ def insert_user(insert_status):
     conn.close()
     return user_id
 
-@pytest.fixture(autouse=True)
+
+@pytest.fixture(scope='session', autouse=True)
 def insert_usergroup(insert_user):
     _, conn = connection()
     conn.begin()
@@ -49,7 +52,8 @@ def insert_usergroup(insert_user):
     conn.close()
     return usergroup_id
 
-@pytest.fixture(autouse=True)
+
+@pytest.fixture(scope='session', autouse=True)
 def insert_notegroup(insert_usergroup, insert_user):
     _, conn = connection()
     conn.begin()
@@ -59,7 +63,7 @@ def insert_notegroup(insert_usergroup, insert_user):
     conn.close()
     return notegroup_id
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def insert_text_note_type(create_db):
     _, conn = connection()
     conn.begin()
@@ -69,7 +73,7 @@ def insert_text_note_type(create_db):
     conn.close()
     return note_type_id
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def insert_note(insert_user, insert_text_note_type, insert_notegroup):
     """Insert new note"""
     con, conn = connection()
@@ -81,4 +85,12 @@ def insert_note(insert_user, insert_text_note_type, insert_notegroup):
 
 def test_user_has_access_to_note(insert_note):
     if has_access_to_note(1, 1) != True:
+        raise AssertionError()
+
+def test_type_id(insert_text_note_type):
+    if 1 != type_id('text'):
+        raise AssertionError()
+
+def test_has_access_to_usergroup(insert_usergroup):
+    if has_access_to_usergroup(1, 1) != True:
         raise AssertionError()
