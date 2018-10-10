f1ed499863afdd6192ed6a548e9f041534b43456
filipw01 <filipw01@interia.eu>
2018-10-10 21:20:28 +0200

    Remove bundles from gitignore to save on installing dev dependencies

--- src/js/NotegroupList.jsx
+++ src/js/NotegroupList.jsx
@@ -3,11 +3,39 @@ import styles from "../scss/test.scss";
 
 export default class NotegroupList extends Component {
   render() {
+    constructor(props) {
+      super(props)
+      this.state = {
 
-      //konstruktor    
-      //fetch wszystkich notegroup dla użytkownika
-      //spakowanie wyników w listę
-    
+      }
+      const that = this;
+      fetch(siteUrl + '/api?query={getUsergroupsOfUser}')
+        .then(response => response.json())
+        .then(myJson => JSON.parse(myJson.data.getContent))
+        .then(function (innerJson) {
+          let folderContent = [];
+          for (const notegroup of innerJson) {
+            let object = {};
+            if (notegroup.idnote) {
+              object["title"] = notegroup.name;
+              object["key"] = "note" + notegroup.idnote;
+              object["is_note"] = true;
+            } else {
+              object["title"] = notegroup.folder_name;
+              object["key"] = notegroup.idnotegroup;
+              object["is_note"] = false;
+            }
+            folderContent.push(object);
+          };
+        })
+        .catch(error => console.log(error));
+    }
+
+
+
+    //fetch wszystkich notegroup dla użytkownika
+    //spakowanie wyników w listę
+
     return (
       <div className={styles.lala}>
         NotegroupList
