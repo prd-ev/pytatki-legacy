8d9d3bee986bacfe2680d0f683a3d12faafa24d8
filipw01 <filipw01@interia.eu>
2018-10-10 11:01:17 +0200

    Add autoprefixer and sass compilation in webpack, add source maps

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Notatki from "./Notatki.jsx";
 import React from "react";
 import ReactDOM from "react-dom";
+import "../scss/test.scss";
 
 ReactDOM.render(<Notatki/>, document.getElementById("reactEntry"));
