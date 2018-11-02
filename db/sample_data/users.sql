9c937ff21ce197e8717062d0f2a842f4e0402770
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-11-02 13:12:10 +0100

    Create sample data

---
+++ db/sample_data/users.sql
@@ -0,0 +1,6 @@
+-- Fri Nov  2 12:59:47 2018
+-- Name: sample_data/users.sql
+-- Author: Patryk Niedźwiedziński
+-- Version: 02.00.0000
+
+INSERT INTO user (1, "test_user", "12345", "test@test.com", 0, 1)
