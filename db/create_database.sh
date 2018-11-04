c7fdb2c5e6988446aaaa2026c060c0178d71201e
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-11-04 22:41:02 +0100

    Fix shell script

--- db/create_database.sh
+++ db/create_database.sh
@@ -1,1 +1,9 @@
-mysql -e 'SET @dbname = \"'+$1+'\"; \. create_database.sql; \. create_tables.sql'
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: create_database.sh db_name";
+  exit 1;
+fi
+
+mysql -e 'SET @dbname = "'$1'"; \. db/create_database.sql'
+mysql -e 'SET @dbname = "'$1'"; \. db/create_tables.sql'
